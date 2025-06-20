5794f3b76c0dc8d969d7f019cb8e1034abf4d95e
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-20 13:16:02 -0300

    Modelagem da classe de usuario

---
+++ src/main/java/br/com/feliperochasi/luminabank/domain/User.java
@@ -0,0 +1,32 @@
+package br.com.feliperochasi.luminabank.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String cpf;
+
+    private String phone;
+
+    private String password;
+
+    @OneToMany
+    private Address address;
+}
